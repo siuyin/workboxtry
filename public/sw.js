f47d8b7972ec6d66604a480640bab6ae82801c7a
Loh Siu Yin <siuyin@beyondbroadcast.com>
2023-04-15 09:44:23 +0000

    prettier 'ed

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,8 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js',
+);
 
-workbox.routing.registerRoute( ({request}) => 
-  request.destination === "document",
-    new workbox.strategies.StaleWhileRevalidate()
+workbox.routing.registerRoute(
+  ({request}) => request.destination === 'document',
+  new workbox.strategies.StaleWhileRevalidate(),
 );
